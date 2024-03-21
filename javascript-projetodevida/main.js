87e447f60f41c587c30f6eb5667e3d9b1132ff1e
sgarbossabeatriz <110116950+sgarbossabeatriz@users.noreply.github.com>
2024-03-21 08:53:59 -0300

    Add files via upload

--- javascript-projetodevida/main.js
+++ javascript-projetodevida/main.js
@@ -1,25 +1,40 @@
 const botoes = document.querySelectorAll(".botao");
-const textos = document.querySelector(".aba-conteudo");
+const textos = document.querySelectorAll(".aba-conteudo");
 
-for(let i=0;i <botoes.length;i++){
-    botoes[i].onclick = function(){
-        
-        for(let j=0;j<botoes.length;j++){
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
+
+        for (let j = 0; j < botoes.length; j++) {
             botoes[j].classList.remove("ativo");
-            botoes[j].classList.remove("ativo");
-        } 
-         
+            textos[j].classList.remove("ativo");
+        }
+
         botoes[i].classList.add("ativo");
-        botoes[i].classList.add("ativo");
-     }   
- }
-     
+        textos[i].classList.add("ativo");
+    }
+}
+
 const contadores = document.querySelectorAll(".contador");
-const tempo0bjetivo1 = new Date("2023-10-05T00:00:00");
-let tempoAtual = new Date();
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+const tempoObjetivo2 = new Date("2023-10-05T00:00:00");
+const tempoObjetivo3 = new Date("2023-10-05T00:00:00");
+const tempoObjetivo4 = new Date("2023-10-05T00:00:00");
 
-contadores[0].textContent = tempoObjetivo1 - tempoAtual;
- 
-    
-    
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
 
+function calculaTempo(tempoObjetivo){
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal/1000);
+    let minutos = Math.floor(segundos/60);
+    let horas = Math.floor(minutos/60);
+    let dias = Math.floor(horas/24);
+    
+    segundos %= 60;
+    minutos%= 60;
+    hoeras%= 24;
+    return dias + "dias" + horas+ " horas " + minutos + " minutos " + segundos + " segundos ";
+
+
+
+}
